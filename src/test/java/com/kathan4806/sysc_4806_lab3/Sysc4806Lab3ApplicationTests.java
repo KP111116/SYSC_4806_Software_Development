4c50059fcaaa79724b3ea0881e594e3056f279ab
Kathan Patel <kathanpatel@cmail.carleton.ca>
2023-10-15 22:13:51 -0400

    Testing base.

--- src/test/java/com/kathan4806/sysc_4806_lab3/Sysc4806Lab3ApplicationTests.java
+++ src/test/java/com/kathan4806/sysc_4806_lab3/Sysc4806Lab3ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.kathan4806.sysc_4806_lab3;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Sysc4806Lab3ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private ABController controller;
 
+    @Test
+    void contextLoads() throws Exception{
+        assertThat(controller).isNotNull();
+    }
 }
